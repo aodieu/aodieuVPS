2b8095c27edb8947fc559fd7eae6e9aa70507251
notrinos <trananhphuong83@gmail.com>
2025-12-04 09:33:44 +0700

    wrong script url for github branch, fixed.

--- scripts/8/install.sh
+++ scripts/8/install.sh
@@ -2,16 +2,16 @@
 #######################################################
 # aodieuVPS Script v1.0 for Rocky Linux 8
 # To install type: 
-# curl -sO https://aodieu.com/script/install && bash install
+# curl -sO https://raw.githubusercontent.com/aodieu/aodieuVPS/github/install && bash install
 # or
-# curl -sO https://aodieu.com/script/$(rpm -E %rhel)/install && bash install
+# curl -sO https://raw.githubusercontent.com/aodieu/aodieuVPS/github/$(rpm -E %rhel)/install && bash install
 #######################################################
 aodieuvps_version="2.1"
-phpmyadmin_version="4.9.5" # Released 2020-03-21. Older version compatible with PHP 5.5 to 7.4 and MySQL 5.5 and newer. Currently supported for security fixes only.
+phpmyadmin_version="5.2.1" # Released 2023-02-08. Compatible with PHP 7.0 to 8.4 and MySQL 5.5 and newer. Actively maintained and receives security updates.
 extplorer_version="2.1.13" # 05/15/2019 04:43 PM
 extplorer_id="82"
-script_root="https://aodieu.com/script/scripts"
-script_url="https://aodieu.com/script/scripts/8"
+script_root="https://raw.githubusercontent.com/aodieu/aodieuVPS/github/scripts"
+script_url="https://raw.githubusercontent.com/aodieu/aodieuVPS/github/scripts/8"
 low_ram='262144' # 256MB
 
 dnf -y install gawk bc wget lsof
